c4bea8c20f91bedf40edbe93c97c42503ffd0618
Krzysztof Piatkowski <krzysztofpiatkowski1987@gmail.com>
2025-04-04 14:39:32 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const getSum = (first) => (second) => {
+    count++;
+
+    if (count <= 3) {
+      return first + second;
+    }
+
+    if (count > 3) {
+      return count % 2 === 0 ? 'Bzzz... Error!' : first + second;
+    }
+  };
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
